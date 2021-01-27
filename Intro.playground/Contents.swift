02c5a5a50ca39ecd9aed696e43d60caac45ba267
m990 <maxn990@gmail.com>
2021-01-26 21:41:16 -0800

    Initial commit - template for hack club lesson

---
+++ Intro.playground/Contents.swift
@@ -0,0 +1,46 @@
+import UIKit
+
+// NOTE - We're using Swift 5.0 and working out of a free ebook provided by Apple to learn the Swift language entitled "The Swift Programming Language (Swift 5.0)"
+
+// VARIABLES - create name, age, concatination, and constants
+
+
+
+// CONTROL FLOW
+// IF STATEMENTS - determine if someone is eligable for a driver's licence
+
+// FOR LOOPS
+let scores = [1, 100, 5, 230, 101, 99, 114]
+// TWO TYPES OF FOR STATEMENTS
+// Use a for each loop to determine is someone won
+
+
+// TYPE TWO - use a standard loop to determine if someone won
+
+
+// NOTE - switch statements work and are nearly identical to other languages so I won't go into them. Just know a break statement isn't necessary
+
+// FUNCTIONS
+// determine if someone wins
+
+
+// find the first winner in an array, return -1 if no one wins
+
+
+
+
+// CLASSES - create a car class
+class Car{
+    // MEMBER VARIABLES - isElectric, make, model
+
+    
+    // CONSTRUCTOR - initialise variables
+
+    
+    // METHODS
+    // NOTE - every class should contain a getter and a setter for each member variable and additional methods to carry out the desired functionality of the class
+}
+
+// INSTANCIATE THE CLASS - run the getters and setters
+
+// OTHER STUFF - (1) generics work when creating functions (2) emojis can be used in variable names (3) semicolins are optional (4) Swift defaults to double (5) Converting to any type is similiar to how we did it above with Strings (6) Swift supports tuples (7) null is represented by nil (8) exception handling differs from most languages: do{ try ... function() } catch {} Throwing exceptions is standard (9) Swift has unary operators (!b), binary operators (2+3) and turnary operators (a?b:c) just like every lanaguage (10) Logic operators are the same as any language (!a, a&&b, a||b)
